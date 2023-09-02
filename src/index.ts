5dc4cf54401b50a349732db5d2aad23594fd607a
Jonatahn Bajada <jbajada@ryerson.ca>
2023-09-01 23:31:35 -0400

    added routes

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,12 @@ dotenv.config()
 import express from 'express'
 const app = express()
 import morgan from 'morgan'
+import { nanoid } from 'nanoid'
 
-// try {
-// 	const response = await fetch(
-// 		'https://www.course-api.com/react-useReducer-cart-project'
-// 	)
-// 	const cartData = await response.json()
-// 	console.log(cartData)
-// } catch (error) {
-// 	console.log(error)
-// }
-
-//fetch example
-// fetch('https://www.course-api.com/react-useReducer-cart-project')
-// 	.then((res) => res.json())
-// 	.then((data) => console.log(data))
+let jobs = [
+	{ id: nanoid(), company: 'apple', position: 'front-end' },
+	{ id: nanoid(), company: 'google', position: 'front-end' },
+]
 
 if (process.env.NODE_ENV === 'development') {
 	app.use(morgan('dev'))
@@ -32,6 +23,35 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
 	console.log(req)
 	res.json({ message: 'data received', data: req.body })
+})
+
+// GET ALL JOBS
+app.get('/api/v1/jobs', (req, res) => {
+	res.status(200).json({ jobs })
+})
+
+// CREATE JOB
+app.post('/api/v1/jobs', (req, res) => {
+	const { company, position } = req.body
+	if (!company || !position) {
+		res.status(400).json({ msg: 'please provide company and position' })
+		return
+	}
+	const id = nanoid(10)
+	const job = { id, company, position }
+	jobs.push(job)
+	res.status(200).json({ job })
+})
+
+// GET SINGLE JOB
+app.get('/api/v1/jobs/:id', (req, res) => {
+	const { id } = req.params
+	const job = jobs.find((job) => job.id === id)
+	if (!job) {
+		res.status(404).json({ msg: `no job with id ${id}` })
+		return
+	}
+	res.status(201).json({ job })
 })
 
 const port = process.env.PORT || 5100
