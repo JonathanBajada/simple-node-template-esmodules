df261b917ff5c7b253c03ed2d9439af1f32c8c04
Jonatahn Bajada <jbajada@ryerson.ca>
2023-09-04 18:43:23 -0400

    fixed config to auto import

--- errors/customErrors.ts
+++ errors/customErrors.ts
@@ -4,6 +4,15 @@ export class NotFoundError extends Error {
 	statusCode: StatusCodes.NOT_FOUND
 	constructor(message) {
 		super(message)
+		this.name = 'NotFoundError'
+		this.statusCode = StatusCodes.NOT_FOUND
+	}
+}
+
+export class BadRequestError extends Error {
+	statusCode: StatusCodes.NOT_FOUND
+	constructor(message) {
+		super(message)
 		this.statusCode = StatusCodes.NOT_FOUND
 	}
 }
